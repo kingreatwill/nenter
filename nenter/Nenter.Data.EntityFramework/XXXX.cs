8f93eb1b1c00cb8444b889a78c7a13a6fd280541
Enter <350840291@qq.com>
2019-09-21 09:51:03 +0800

    IQueryable ToSql

---
+++ nenter/Nenter.Data.EntityFramework/XXXX.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Data.Entity.Core.Objects;
+namespace Nenter.Data.EntityFramework
+{
+    // https://github.com/aspnet/EntityFrameworkCore/issues/6482
+    public static class XXXX
+    {
+        public static string ToSql<TEntity>(this IQueryable<TEntity> queryable)
+            where TEntity : class
+        {
+            System.Data.Entity.Core.Objects.ObjectQuery
+            string sql = ((System.Data.Objects.ObjectQuery)customerNames).ToTraceString();
+ 
+            Console.WriteLine(sql);
+        }
+
+       
+    }
+}
